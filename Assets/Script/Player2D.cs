408c692b06c6df00d805a0815462881260d5eb8c
sariyachanh-dotcom <sariya.chanh@bumail.net>
2026-05-05 20:07:06 +0700

    Create Script Player2D

---
+++ Assets/Script/Player2D.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Player2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float runSpeed = 8f;
+    public float jumpForce = 15f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float move = Input.GetAxis("Horizontal");
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
